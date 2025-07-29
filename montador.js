d9ca804ac9d9f328fc80bcc7e51a9282cfa8f1c0
Pedro José C.A <pedrojos.campos@protonmail.com>
2025-07-29 14:51:52 -0300

    cria elementos de disco estilizados

--- montador.js
+++ montador.js
@@ -1,8 +1,20 @@
-criaTabuleiro();
+const tabuleiro = criaTabuleiro();
+document.body.append(tabuleiro);
 
+const disco1 = criaDisco("branco")
+tabuleiro.appendChild(disco1);
+const disco2 = criaDisco("preto")
+tabuleiro.appendChild(disco2);
 
 function criaTabuleiro() {
     const eTabuleiro = document.createElement("div");
     eTabuleiro.classList.add("tabuleiro");
-    document.body.append(eTabuleiro);
+    return eTabuleiro;
+}
+
+function criaDisco(cor) {
+    const eDisco = document.createElement("div");
+    eDisco.classList.add("disco");
+    eDisco.dataset.cor = cor;
+    return eDisco;
 }
